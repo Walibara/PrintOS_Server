fef02b1835598a738b3968e33c7ee9953a696263
esturm18 <emma.sturm99@gmail.com>
2025-10-22 21:51:14 -0700

    Editing Test class again to have import statement of main class

--- src/test/java/com/example/printos_server/PrintosServerApplicationTests.java
+++ src/test/java/com/example/printos_server/PrintosServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.printos_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.capstone.printos_server.PrintosServerApplication; //Emma - importing in main class for test (otherwise github action failure) 10/22
 
 @SpringBootTest(classes = PrintosServerApplication.class)
 class PrintosServerApplicationTests {
