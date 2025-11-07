617a9a7c7ceccf70313978d7aeda1bb03fdad66d
CIELOARGE <158418246+CIELOARGE@users.noreply.github.com>
2025-11-07 09:55:18 -0800

    Update HelloController.java

--- src/main/java/com/capstone/printos_server/HelloController.java
+++ src/main/java/com/capstone/printos_server/HelloController.java
@@ -1,4 +1,4 @@
-package com.capstone.printos_server;
+package com.capstone.printos_server.config;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
