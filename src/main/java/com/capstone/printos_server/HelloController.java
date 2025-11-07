2ac262c54a53ec5250260ece55b5ca26a7ae9df6
CIELOARGE <158418246+CIELOARGE@users.noreply.github.com>
2025-11-06 20:18:03 -0800

    Update HelloController.java

--- src/main/java/com/capstone/printos_server/HelloController.java
+++ src/main/java/com/capstone/printos_server/HelloController.java
@@ -1,23 +1,13 @@
 package com.capstone.printos_server;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(
-    //Adding more origins - Maria 
-    origins = {
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://main.d22sjrfdf1uqnw.amplifyapp.com",
-        "https://dkdavnbhgrmho.cloudfront.net"
-    }
-)
 @RestController
 public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot - 11/6/2025!";
     }
 }
