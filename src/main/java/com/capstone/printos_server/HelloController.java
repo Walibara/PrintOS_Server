bcf055f06455d7403661c4bd347776c3429b7dcd
CIELOARGE <158418246+CIELOARGE@users.noreply.github.com>
2025-11-06 20:08:06 -0800

    Update HelloController.java
    Adding more origins to see if it will work with frontend

--- src/main/java/com/capstone/printos_server/HelloController.java
+++ src/main/java/com/capstone/printos_server/HelloController.java
@@ -4,7 +4,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://localhost:5173"}) 
+@CrossOrigin(
+    //Adding more origins - Maria 
+    origins = {
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://main.d22sjrfdf1uqnw.amplifyapp.com",
+        "https://dkdavnbhgrmho.cloudfront.net"
+    }
+)
 @RestController
 public class HelloController {
 
