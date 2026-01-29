208d7c4025ddb0a347d0c2d69aa6d98f4d4a532a
CIELOARGE <158418246+CIELOARGE@users.noreply.github.com>
2026-01-28 23:12:57 -0800

    your new commit message here

--- src/main/java/com/capstone/printos_server/HelloController.java
+++ src/main/java/com/capstone/printos_server/HelloController.java
@@ -2,13 +2,18 @@ package com.capstone.printos_server;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello from Spring Boot - 11/7/2025!";
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+        
+        return "Hello from Spring Boot! Let's display local time to make sure we are working: " + formattedDateTime;
     }
 }
-
