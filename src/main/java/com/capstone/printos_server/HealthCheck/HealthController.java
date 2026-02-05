cb697f3871c3873774ae7bdecaf513f38662fce6
esturm18 <emma.sturm99@gmail.com>
2026-02-04 21:10:35 -0800

    One Day

--- src/main/java/com/capstone/printos_server/HealthCheck/HealthController.java
+++ src/main/java/com/capstone/printos_server/HealthCheck/HealthController.java
@@ -1,11 +1,16 @@
+package com.capstone.printos_server.health;
+
+import com.capstone.printos_server.jobs.Job;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+
 
 @RestController
 public class HealthController {
@@ -20,14 +25,15 @@ public class HealthController {
     public ResponseEntity<Map<String, Object>> getHealth() {
         Map<String, Object> response = new HashMap<>();
         try {
-            LocalDateTime dbTime = statusRepo.getDatabaseTime();
-            response.put("timestamp", dbTime.toString());
+            Timestamp dbTime = healthRepo.getDatabaseTime();
+            LocalDateTime localDbTime = dbTime.toLocalDateTime();
+            response.put("timestamp", localDbTime.toString());
             response.put("message", "found");
-            return ResponseEntity.ok(response);  //HTTP 200 good :)
+            return ResponseEntity.ok(response);  //HTTP 200
         } catch (Exception e) {
             response.put("timestamp", LocalDateTime.now().toString());
             response.put("message", "not found");
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response); //HTTP 503 bad:(
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response); //HTTP 503
         }
     }
 }
