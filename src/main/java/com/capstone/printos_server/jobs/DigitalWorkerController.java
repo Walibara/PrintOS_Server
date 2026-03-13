ea0bbf2eec81b798d57228c4ed2fa2a341879855
esturm18 <151462649+esturm18@users.noreply.github.com>
2026-03-12 21:10:00 -0700

    Update DigitalWorkerController.java

--- src/main/java/com/capstone/printos_server/jobs/DigitalWorkerController.java
+++ src/main/java/com/capstone/printos_server/jobs/DigitalWorkerController.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dw/jobs")
