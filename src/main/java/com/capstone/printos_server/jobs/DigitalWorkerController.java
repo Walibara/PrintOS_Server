ada464510c992b24b29f06972757803f7ff9f6a5
esturm18 <emma.sturm99@gmail.com>
2026-04-11 14:24:08 -0700

    Single job

--- src/main/java/com/capstone/printos_server/jobs/DigitalWorkerController.java
+++ src/main/java/com/capstone/printos_server/jobs/DigitalWorkerController.java
@@ -19,6 +19,8 @@ public class DigitalWorkerController {
         this.repo = repo;
     }
 
+    
+
     // Emma - Heartbeat, return timestamp for a job
      @PutMapping("/{id}/heartbeat")
      public ResponseEntity<String> heartbeat(@PathVariable Long id) {
